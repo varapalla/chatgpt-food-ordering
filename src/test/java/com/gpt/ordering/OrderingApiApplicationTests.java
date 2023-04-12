310f02a84fb1bb826ce635ee2642e99c407188ad
tanujoberoi <er.tanujoberoi@gmail.com>
2023-04-12 13:11:28 +0530

    Add existing project files to Git

---
+++ src/test/java/com/gpt/ordering/OrderingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpt.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
